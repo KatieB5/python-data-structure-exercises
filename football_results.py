a4d1a3c5b2e0761ee9afe3a2b239bef732d43c64
Katie Bickford <katie.bickford@thedatalab.org>
2025-06-12 16:12:00 +0100

    Update code to display answer to 3rd and 4th questions

--- football_results.py
+++ football_results.py
@@ -18,45 +18,65 @@ results = [
         {'Hungary': 3, 'Portugal': 3},
 ]
 
+unique_teams = {k for dict in results for k in dict.keys()}
+
+team_goals = {team: 0 for team in unique_teams}
+
+
 def main():
 #match goals
-        most_goals, fewest_goals = get_match_goals()
+     match_most_goals, match_fewest_goals = get_match_goals()
 #team goals
-
+     team_most_goals, team_fewest_goals = get_team_goals()
 #team points
 
 
 # TODO: Write code to answer the following questions:
 
-        print(f'The match with the most goals was {most_goals}')
-        print(f'The match with the fewest goals was {fewest_goals}')
-        print('The team with the most total goals was', '?')
-        print('The team with the fewest total goals was', '?')
-        print('The team with the most points was', '?')
-        print('The team with the fewest points was', '?')
+     print(f'The match with the most goals was {match_most_goals}')
+     print(f'The match with the fewest goals was {match_fewest_goals}')
+     print(f'The team with the most total goals was {team_most_goals}')
+     print(f'The team with the fewest total goals was {team_fewest_goals}?')
+     print('The team with the most points was', '?')
+     print('The team with the fewest points was', '?')
 
 def get_match_goals():
-      most_goals_count = get_goal_total(0)
-      most_goals_match = results[0]
-      fewest_goals_count = get_goal_total(0)
-      fewest_goals_match = results[0]
+     most_goals_count = get_goal_total(0)
+     match_most_goals = results[0]
+     fewest_goals_count = get_goal_total(0)
+     match_fewest_goals = results[0]
 
-      for i in range(1, len(results)):
-        goal_total = get_goal_total(i)
-        if goal_total > most_goals_count:
-             most_goals_count = goal_total
-             most_goals_match = results[i]
-        if goal_total < fewest_goals_count:
-             fewest_goals_count = goal_total
-             fewest_goals_match = results[i]
+     for i in range(1, len(results)):
+          goal_total = get_goal_total(i)
+          if goal_total > most_goals_count:
+               most_goals_count = goal_total
+               match_most_goals = results[i]
+          if goal_total < fewest_goals_count:
+               fewest_goals_count = goal_total
+               match_fewest_goals = results[i]
 
-      return [format_match(most_goals_match), format_match(fewest_goals_match)]
+     return [format_match(match_most_goals), format_match(match_fewest_goals)]
 
 def get_goal_total(i):
      return reduce(lambda x, y: x+y, results[i].values())
 
 def format_match(match_obj):
      return f"{list(match_obj.keys())[0]} vs {list(match_obj.keys())[1]}"
+
+def get_team_goals():
+     for i in range(len(results)):
+          teams = list(results[i].keys())
+          goals = list(results[i].values())
+
+          team_goals[teams[0]] += goals[0]
+          team_goals[teams[1]] += goals[1]
+
+          sorted_team_goals = {k: v for k, v in sorted(team_goals.items(), key=lambda item: item[1])}
+
+          team_most_goals = list(sorted_team_goals.keys())[len(sorted_team_goals) - 1]
+          team_fewest_goals = list(sorted_team_goals.keys())[0]
+
+     return [team_most_goals, team_fewest_goals]
 
 if __name__ == "__main__":
     main()
