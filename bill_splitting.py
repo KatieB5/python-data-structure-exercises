f1f1cd6bae4b68f49c128868bed03e34a2262897
Katie Bickford <katie.bickford@thedatalab.org>
2025-07-03 12:36:20 +0100

    Update if __name__ call and add arg parser

--- bill_splitting.py
+++ bill_splitting.py
@@ -15,28 +15,46 @@
 # $ python bill_splitting.py Tim
 # Tim did not have dinner
 
+import argparse
 
 bill_items = [
-    ['Tom', 'Calamari', 6.00],
-    ['Tom', 'American Hot', 11.50],
-    ['Tom', 'Chocolate Fudge Cake', 4.45],
-    ['Clare', 'Bruschetta Originale', 5.35],
-    ['Clare', 'Fiorentina', 10.65],
-    ['Clare', 'Tiramasu', 4.90],
-    ['Rich', 'Bruschetta Originale', 5.35],
-    ['Rich', 'La Reine', 10.65],
-    ['Rich', 'Honeycomb Cream Slice', 4.90],
-    ['Rosie', 'Garlic Bread', 4.35],
-    ['Rosie', 'Veneziana', 9.40],
-    ['Rosie', 'Tiramasu', 4.90],
+    ["Tom", "Calamari", 6.00],
+    ["Tom", "American Hot", 11.50],
+    ["Tom", "Chocolate Fudge Cake", 4.45],
+    ["Clare", "Bruschetta Originale", 5.35],
+    ["Clare", "Fiorentina", 10.65],
+    ["Clare", "Tiramasu", 4.90],
+    ["Rich", "Bruschetta Originale", 5.35],
+    ["Rich", "La Reine", 10.65],
+    ["Rich", "Honeycomb Cream Slice", 4.90],
+    ["Rosie", "Garlic Bread", 4.35],
+    ["Rosie", "Veneziana", 9.40],
+    ["Rosie", "Tiramasu", 4.90],
 ]
 
-print('There are {} items on the bill'.format(len(bill_items)))
 
 def main():
-    pass
+    name = get_args()
 
-if __name__ == "__main":
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        prog="Bill Splitting",
+        description="This program reports how much individuals should pay for their order at dinner",
+    )
+
+    parser.add_argument(
+        "name",
+        help="The name of the person who's total order amount you want",
+        nargs="?",
+        default="",
+        type=str,
+    )
+
+    return vars(parser.parse_args())
+
+
+if __name__ == "__main__":
     main()
 
 # TODO:
