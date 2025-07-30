d36ee3298c1bb2ec4657adcc49515ada4328ad93
Katie Bickford <katie.bickford@thedatalab.org>
2025-07-30 16:41:56 +0100

    Organise president data into a list of namedtuples
    Flatten the provided data structure into a list of data elements. First considered a list of dicts, but then opted for a list of namedtuples:
    - More memory efficient vs dicts
    - Still allow named attribute access
    - Immutable

--- presidents.py
+++ presidents.py
@@ -2,8 +2,24 @@ from datetime import date
 
 from presidents_data import presidents_by_party
 
+from collections import namedtuple
+
 print('There have been presidents from {} different parties'.format(len(presidents_by_party)))
 
+President = namedtuple('President', [
+    'name', 'party', 'born', 'took_office', 'left_office'
+])
+
+presidents = []
+
+for party, presidents_list in presidents_by_party.items():
+    for president in presidents_list:
+        president = President(name = president["name"], party=party,
+                born=president["born"],
+                took_office=president["took_office"],
+                left_office=president["left_office"]
+            )
+        presidents.append(president)
 
 # TODO:
 # * Display a report that answers the following questions:
