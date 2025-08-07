3f643e20c3b9e2b1bc7ec08d013233c3a31458c1
Katie Bickford <katie.bickford@thedatalab.org>
2025-08-07 12:50:31 +0100

    Update script to retrieve first 5 answers

--- presidents.py
+++ presidents.py
@@ -1,29 +1,66 @@
-from datetime import date
+from datetime import date, datetime
 
 from presidents_data import presidents_by_party
 
-from collections import namedtuple
+from collections import namedtuple, Counter
 
 def main():
-    presidents = get_presidents_data(presidents_by_party)
+    president_namedtuples = get_presidents_data(presidents_by_party)
+    party = get_party_with_most_presidents(president_namedtuples)
+    youngest_rep = get_est_pres(president_namedtuples, "youngest", "Republican")
+    oldest_dem = get_est_pres(president_namedtuples, "oldest", "Democratic")
+    youngest_pres = get_est_pres(president_namedtuples, "youngest")
+    oldest_pres = get_est_pres(president_namedtuples, "oldest")
+    output_message(party, youngest_rep, oldest_dem, youngest_pres, oldest_pres)
 
-def get_presidents_data(presidents_data):
+def get_presidents_data(data_dict):
     President = namedtuple('President', [
-        'name', 'party', 'born', 'took_office', 'left_office'
+        'name', 'party', 'born', 'took_office', 'took_office_age', 'left_office'
     ])
 
     presidents = []
 
-    for party, presidents_list in presidents_data.items():
+    for party, presidents_list in data_dict.items():
         for president in presidents_list:
-            president = President(name = president["name"], party=party,
-                    born=president["born"],
-                    took_office=president["took_office"],
-                    left_office=president["left_office"]
-                )
+            born_date = president["born"]
+            took_office_date = president["took_office"]
+            age_at_office = (took_office_date - born_date).days // 365
+
+            president = President(
+                name = president["name"],
+                party=party,
+                born=president["born"],
+                took_office=president["took_office"],
+                took_office_age=age_at_office,
+                left_office=president["left_office"]
+            )
+
             presidents.append(president)
 
     return presidents
+
+def get_party_with_most_presidents(presidents):
+    """Create a Counter object to then use most_common([n]) to return a
+    list with a tuple containing the most common presidential party and
+    the count. The access and return the name of the party."""
+    party_counts = Counter(p.party for p in presidents)
+    return party_counts.most_common(1)[0][0]
+
+def get_est_pres(presidents, youngest_or_oldest, pres_party=None):
+    """Get a list of all presidents for a given party, then return the
+    name of the oldest or youngest president depending on the args
+    passed into the function. If no party is specified, return the
+    oldest or youngest president overall"""
+    if pres_party is None:
+        party_presidents = presidents
+    else:
+        party_presidents = [p for p in presidents if p.party == pres_party]
+
+    min_max = min if youngest_or_oldest == "youngest" else max
+    return min_max(party_presidents, key=lambda p: p.took_office_age).name
+
+def output_message(*args):
+    print(args)
 
 
 if __name__ == "__main__":
