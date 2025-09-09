003aed4d18ffb4784e852de15ce9255aa74d95d6
Katie Bickford <katie.bickford@thedatalab.org>
2025-09-09 09:14:17 +0100

    Add functionality to answer >50% market share question

--- browser_stats.py
+++ browser_stats.py
@@ -15,7 +15,8 @@ BrowserRecord = namedtuple(
 def main():
     browser_tuples = get_browser_tuples(browser_stats_by_year_and_month)
     reporting_period = get_reporting_period(browser_tuples)
-    display_report(reporting_period)
+    market_share_over_50 = get_over_50_market_share_browsers(browser_tuples)
+    display_report(reporting_period, market_share_over_50)
 
 
 def get_browser_tuples(data):
@@ -38,9 +39,19 @@ def get_reporting_period(data_tuples):
     reporting_period = f"{data_tuples[-1].month} {data_tuples[-1].year} to {data_tuples[0].month} {data_tuples[0].year}"
     return reporting_period
 
-def display_report(reporting_period):
+def get_over_50_market_share_browsers(data_tuples):
+    browser_list = []
+    for browser in data_tuples:
+        if browser.market_share > 50 and browser.browser not in browser_list:
+            browser_list.append(browser.browser)
+
+    return browser_list
+
+
+def display_report(reporting_period, market_share_over_50):
     print("A report of browser usage statistics, as recorded by w3schools.com:")
-    print(f"This report covers the period{reporting_period}")
+    print(f"This report covers the period {reporting_period}")
+    print(f"The following browsers had had over 50% of market share: {', '.join(market_share_over_50)}")
 
 if __name__ == "__main__":
     main()
